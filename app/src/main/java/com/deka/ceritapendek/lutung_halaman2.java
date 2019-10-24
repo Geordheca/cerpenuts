0a2461f4a4cf39caeb76d143a21d591620839017
Geordheca <50356713+Geordheca@users.noreply.github.com>
2019-10-25 00:16:27 +0700

    Initial commit

---
+++ app/src/main/java/com/deka/ceritapendek/lutung_halaman2.java
@@ -0,0 +1,14 @@
+package com.deka.ceritapendek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class lutung_halaman2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lutung_halaman2);
+    }
+}
